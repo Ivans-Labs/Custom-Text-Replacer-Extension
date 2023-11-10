9015006f01870676e7320f557358f0ea1e74e9b8
Ivan Ryan <ivan@ivanryan.dev>
2023-11-10 03:16:30 +0100

    fix: searching

--- js/content.js
+++ js/content.js
@@ -1,16 +1,27 @@
 // content.js
 function replaceTextOnPage(replacements) {
-  const bodyText = document.body.innerHTML;
-  let modifiedText = bodyText;
+  // Function to safely escape regular expression special characters
+  function escapeRegExp(string) {
+    return string.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+  }
 
-  replacements.forEach((replacement) => {
-    const { originalText, newText } = replacement;
-    const escapedOriginalText = originalText.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
-    const regex = new RegExp(escapedOriginalText, 'gi');
-    modifiedText = modifiedText.replace(regex, newText);
-  });
+  // Function to recursively replace text within text nodes
+  function findAndReplaceText(node) {
+    if (node.nodeType === Node.TEXT_NODE) {
+      let text = node.nodeValue;
+      replacements.forEach((replacement) => {
+        const escapedOriginalText = escapeRegExp(replacement.originalText);
+        const regex = new RegExp(escapedOriginalText, 'gi');
+        text = text.replace(regex, replacement.newText);
+      });
+      node.nodeValue = text;
+    } else if (node.nodeType === Node.ELEMENT_NODE) {
+      node.childNodes.forEach(findAndReplaceText);
+    }
+  }
 
-  document.body.innerHTML = modifiedText;
+  // Start the replacement process from the body of the page
+  findAndReplaceText(document.body);
 }
 
 // Named function for handling storage retrieval
